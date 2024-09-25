41e52f0b3a42b90c688ea30f3eb353e2728866a7
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-09-25 17:40:54 +0600

    handle show modal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,24 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import CardModal from "./CardModal";
 
 const Card = ({ image, title, description }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleCardClick = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <>
-            <div className="border rounded-lg shadow-lg p-4 cursor-pointer hover:shadow-2xl transition-transform duration-300 transform hover:scale-105 h-[420px]">
+            <div
+                className="border rounded-lg shadow-lg p-4 cursor-pointer hover:shadow-2xl transition-transform duration-300 transform hover:scale-105 h-[420px]"
+                onClick={handleCardClick()}
+            >
                 <img
                     src={image}
                     alt={title}
@@ -12,6 +27,15 @@ const Card = ({ image, title, description }) => {
                 <h3 className="text-xl font-semibold mb-2">{title}</h3>
                 <p className="text-gray-600">{description}</p>
             </div>
+
+            {/* Modal */}
+            <CardModal
+                isOpen={isModalOpen}
+                onClose={handleCloseModal}
+                image={image}
+                title={title}
+                description={description}
+            />
         </>
     );
 };
